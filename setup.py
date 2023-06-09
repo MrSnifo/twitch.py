88e1dbc1e731645549e8bb6e0476bc7351d06e81
SniFo <72699591+MrSniFo@users.noreply.github.com>
2023-06-09 13:53:32 +0100

    Add files via upload
    Initial development stage

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='twitchify',
+    version='0.0.0',
+    packages=['twitch'],
+    author='Snifo',
+    author_email='Snifo@mail.com',
+    description='A Python Twitch API wrapper',
+    url='https://github.com/mrsnifo/twitchify',
+    install_requires=[],
+    license='MIT',
+)
