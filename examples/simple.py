0035afb17ab229762ee6b8d3aa2d2cf115b02416
SniFo <72699591+MrSniFo@users.noreply.github.com>
2023-06-22 23:15:46 +0100

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -3,8 +3,8 @@ from twitch.user import Follower
 
 # Create a Twitch client
 client = Client(
-    client_id="CLIENT ID HERE",
-    client_secret="CLIENT SECRET HERE"
+    client_id='CLIENT ID HERE',
+    client_secret='CLIENT SECRET HERE'
 )
 
 
@@ -13,7 +13,7 @@ async def on_connect():
     """
     Event handler triggered when the client successfully connected to the eventsub websocket.
     """
-    print("Connecting...")
+    print('Connected to the Websocket!')
 
 
 @client.event
@@ -21,7 +21,7 @@ async def on_ready():
     """
     Event handler triggered when the client is ready to start processing events.
     """
-    print("Ready as %s" % client.user.display_name)
+    print(f"Ready as {client.user.display_name}")
 
 
 @client.event
@@ -29,7 +29,8 @@ async def on_refresh_token(access_token: str):
     """
     Event handler triggered when the client receives a new access token.
 
-    Note: The refresh_token and client_secret are required for the on_refresh_token event to trigger.
+    Note:
+        The refresh_token and client_secret are required for the on_refresh_token event to trigger.
     """
     # Store this access_token for future use.
     print("Received a new access token:", access_token)
@@ -40,9 +41,9 @@ async def on_follow(user: Follower):
     """
     Event handler triggered when a user follows the channel.
     """
-    print("%s just followed you!" % user.display_name)
+    print(f'{user.display_name} just followed you!')
 
 
 client.run(
-    access_token="USER ACCESS TOKEN HERE",
-    refresh_token="USER REFRESH TOKEN HERE")
+    access_token='USER ACCESS TOKEN HERE',
+    refresh_token='USER REFRESH TOKEN HERE')
