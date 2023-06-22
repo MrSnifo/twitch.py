0e0f5a540945d8e5009c9a81d9c0d5a7259b4f8f
SniFo <72699591+MrSniFo@users.noreply.github.com>
2023-06-22 23:16:53 +0100

    Update advenced.py

--- examples/advenced.py
+++ examples/advenced.py
@@ -8,20 +8,20 @@ class Twitch(Client):
     """
 
     def __init__(self):
-        super().__init__(client_id="CLIENT ID HERE",
-                         client_secret="CLIENT SECRET HERE")
+        super().__init__(client_id='CLIENT ID HERE',
+                         client_secret='CLIENT SECRET HERE')
 
     async def on_connect(self):
         """
         Event handler triggered when the client successfully connects to the eventsub websocket.
         """
-        print(f"Connected as {self.user.name} ID: {self.user.id}")
+        print(f'Connected as {self.user.name} ID: {self.user.id}')
 
     async def on_ready(self):
         """
         Event handler triggered when the client is ready to start processing events.
         """
-        print(f"Ready as {self.user.display_name}")
+        print(f'Ready as {self.user.display_name}')
 
     async def on_refresh_token(self, access_token: str):
         """
@@ -30,21 +30,21 @@ class Twitch(Client):
         Note: The refresh_token and client_secret are required.
         """
         # Store this access_token for future use.
-        print("Received a new access token:", access_token)
+        print('Received a new access token:', access_token)
 
     async def on_cheer(self, cheer: Cheer):
         """
         Event handler triggered when a user sends a cheer to the channel.
         """
-        print(f"{cheer.user.display_name} just cheered {cheer.bits} bits!")
+        print(f'{cheer.user.display_name} just cheered {cheer.bits} bits!')
 
     def run_client(self):
         """
         You can import the access token here and use it.
         """
         self.run(
-            access_token="USER ACCESS TOKEN HERE",
-            refresh_token="USER REFRESH TOKEN HERE"
+            access_token='USER ACCESS TOKEN HERE',
+            refresh_token='USER REFRESH TOKEN HERE'
         )
 
 
