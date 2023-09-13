b51908fc0cc7e5d9ecd5f97350afe8db57dbc929
SniFo <72699591+MrSniFo@users.noreply.github.com>
2023-09-13 01:18:30 +0100

    update examples

--- examples/simple.py
+++ examples/simple.py
@@ -33,9 +33,7 @@ async def on_follow(user: Follow):
 
 # Method 1: Using access_token and refresh_token
 # Replace with your actual access token and refresh token
-access_token = "YOUR_ACCESS_TOKEN"
-refresh_token = "YOUR_REFRESH_TOKEN"
-client.run(access_token=access_token, refresh_token=refresh_token)
+client.run(access_token="YOUR_ACCESS_TOKEN", refresh_token="YOUR_REFRESH_TOKEN")
 
 # Method 2: You can simply use this method for mainly authentication (scopes are optional)
 # client.run(scopes=['moderator:read:followers'])
