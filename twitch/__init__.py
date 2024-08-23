7d16f59c986e4ce8f317fc7e080c0e360e294a3c
Snifo <snifo@mail.com>
2024-08-23 05:34:57 +0100

    bump v3.0.4

--- twitch/__init__.py
+++ twitch/__init__.py
@@ -8,7 +8,7 @@ Real-time Twitch Event Handling and Helix API Integration in Python
 """
 
 __title__ = 'Twitchify'
-__version__ = '3.0.3'
+__version__ = '3.0.4'
 __license__ = 'MIT License'
 __author__ = 'Snifo'
 __email__ = 'Snifo@mail.com'
