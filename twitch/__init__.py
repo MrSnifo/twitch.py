15eda7ccf6fb363ee753d9c23b6e17ce7f6601b9
Snifo <snifo@mail.com>
2025-01-18 20:01:04 +0100

    bump
    https://github.com/MrSnifo/twitch.py/commit/16aeb30ef5bc98a7263ccddde2de36baed9c45a6

--- twitch/__init__.py
+++ twitch/__init__.py
@@ -8,7 +8,7 @@ Real-time Twitch Event Handling and Helix API Integration in Python
 """
 
 __title__ = 'twitch.py'
-__version__ = '3.2.6'
+__version__ = '3.2.7'
 __license__ = 'MIT License'
 __author__ = 'Snifo'
 __email__ = 'Snifo@mail.com'
